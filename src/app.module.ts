7adfd776216909de7ff5dbb8b81ba4e0505e635c
sharkedd <jose.carrillo@alumnos.ucn.cl>
2025-01-08 17:11:39 -0300

    Implementación de alertas por caída y envío por gmail

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HealthcheckModule } from './healthcheck/healthcheck.module';
+import { ConfigModule } from '@nestjs/config';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Hacer el módulo global
+    }),
+    HealthcheckModule,
+    EmailModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
