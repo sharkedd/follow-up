1b4fa55d223ace074d01c25914f49da89b4a9ace
sharkedd <jose.carrillo@alumnos.ucn.cl>
2025-01-15 09:07:16 -0300

    implementación de playwrigth test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,16 @@
 import { test, expect } from '@playwright/test';
 
+test('practica', async ({ page }) => {
+  await page.goto('https://clientes.orbyta.com/');
+  await expect(page).toHaveTitle(/Orbyta/);
+
+  await page.fill('input[name="usuario"]', 'jcarrillo@follow-up.cl');
+  await page.fill('input[name="pass"]', 'jose.2025!');
+  await page.click('button[type="submit"]');
+  await expect(page.locator('h3')).toHaveText('Bienvenid@ jose carrillo');
+});
+
+/*
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
@@ -14,5 +25,9 @@ test('get started link', async ({ page }) => {
   await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(
+    page.getByRole('heading', { name: 'Installation' }),
+  ).toBeVisible();
 });
+
+*/
